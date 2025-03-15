ad83839b05e7db07691d26634c43894bfc05590c
“myatthin12” <myatthinkhaing1234@gmail.com>
2025-03-15 15:50:47 +0630

    Visual Regression Testing

---
+++ tests/test.js
@@ -0,0 +1,1 @@
+document.querySelector('.thumbBox.imgLarge img')
---
+++ tests/visualRegression.spec.js
@@ -0,0 +1,14 @@
+import {test, expect } from "@playwright/test";
+
+//only PNG
+test('Animate Caravan',async({page})=>{
+    await page.goto('https://animate-caravan.loftal-dev.com')
+    const homepagePic = page.locator('//*[@id="topAboutBox"]/div/div[1]/img');
+    await expect(homepagePic).toHaveScreenshot('Visual.png')
+})
+
+test.only('Div Animate Caravan',async({page})=>{
+    await page.goto('https://animate-caravan.loftal-dev.com');
+    const divPic = page.locator('//*[@id="topAboutBox"]/div/div[1]')
+    await expect(divPic).toHaveScreenshot('DivPic.png')
+})
